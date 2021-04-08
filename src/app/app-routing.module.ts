e67e8f6ef51b2f34d4372b161124b64c9eef9219
Sven Moon <svenpire@gmail.com>
2021-04-08 16:25:31 -0400

    specialty template

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,31 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BuilderComponent } from './modules/order/builder/builder.component';
+import { OrderListComponent } from './modules/order/order-list/order-list.component';
+import { SpecialtyComponent } from './modules/order/specialty/specialty.component';
+import { SsselctorComponent } from './modules/order/ssselctor/ssselctor.component';
 import { HomeComponent } from './modules/pages/home/home.component';
+import { PaymentModule } from './modules/payment/payment.module';
+import { PostPayComponent } from './modules/payment/post-pay/post-pay.component';
 
 const routes: Routes = [
-  { path: '', component: HomeComponent }
+  { path: '', component: HomeComponent },
+  {
+    path: 'order', component: SsselctorComponent,
+    children: [
+      { path: 'specialty', component: SpecialtyComponent },
+      { path: 'builder', component: BuilderComponent },
+      { path: 'my-items', component: OrderListComponent }
+    ]
+  },
+
+  {
+    path: 'payment', component: PaymentModule,
+    children: [
+      { path: 'pay', component: PaymentModule },
+      { path: 'post-pay', component: PostPayComponent }
+    ]
+  }
 ];
 
 @NgModule({
