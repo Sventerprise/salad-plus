53acae9f41264661cff1f772b44276ffa3fe56d9
Sven Moon <svenpire@gmail.com>
2021-04-16 10:15:08 -0400

    order-list popup

--- src/app/modules/order/order-list/order-list.component.ts
+++ src/app/modules/order/order-list/order-list.component.ts
@@ -6,10 +6,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./order-list.component.scss']
 })
 export class OrderListComponent implements OnInit {
+  confirmFlag: boolean = false
+  popupFlag: boolean = false
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  public openCancelConfirm() {
+    this.popupFlag = true
+    this.confirmFlag = true
+  }
+
+  public confirmCancel() {
+  }
+
+  public closePopup() {
+    this.popupFlag = false
+    this.confirmFlag = false
+  }
 }
