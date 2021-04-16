9cbfeff4ce8330fc6fde206d792a296b0985bb4d
Sven Moon <svenpire@gmail.com>
2021-04-16 09:40:11 -0400

    post-pay complete

--- src/app/modules/payment/post-pay/post-pay.component.ts
+++ src/app/modules/payment/post-pay/post-pay.component.ts
@@ -6,6 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./post-pay.component.scss']
 })
 export class PostPayComponent implements OnInit {
+  items = [
+    { qty: 1, description: "custom sandwich 1", price: 68.80, subtotal: 68.80 },
+    { qty: 30, description: "Sourdough Ham Sandwich", price: 268.88, subtotal: 268.88 },
+  ]
+  paySuccessFlag: boolean = true;
 
   constructor() { }
 
