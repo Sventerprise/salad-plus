af6fa6682eb15292cd69819d8ae51c3cbf20d589
Sven Moon <svenpire@gmail.com>
2021-04-16 12:04:19 -0400

    ngrx++ installs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,11 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app/app.component';
 import { AppComponents } from './barrels/app-components';
 import { AppModules } from './barrels/app-modules';
+import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from '../environments/environment';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreRouterConnectingModule } from '@ngrx/router-store';
 
 @NgModule({
   declarations: [
@@ -12,7 +17,11 @@ import { AppModules } from './barrels/app-modules';
   imports: [
     BrowserModule,
     ...AppModules,
-    AppRoutingModule
+    AppRoutingModule,
+    StoreModule.forRoot({}, {}),
+    StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: environment.production }),
+    EffectsModule.forRoot([]),
+    StoreRouterConnectingModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
