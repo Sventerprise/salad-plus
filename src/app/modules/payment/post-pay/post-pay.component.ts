538fb5859849949a3bcd3ccc7cc8580eb859cbb6
Sven Moon <svenpire@gmail.com>
2021-04-16 09:51:28 -0400

    post-pay confirm cancel popup

--- src/app/modules/payment/post-pay/post-pay.component.ts
+++ src/app/modules/payment/post-pay/post-pay.component.ts
@@ -10,11 +10,26 @@ export class PostPayComponent implements OnInit {
     { qty: 1, description: "custom sandwich 1", price: 68.80, subtotal: 68.80 },
     { qty: 30, description: "Sourdough Ham Sandwich", price: 268.88, subtotal: 268.88 },
   ]
-  paySuccessFlag: boolean = true;
+  paySuccessFlag: boolean = false;
+  confirmFlag: boolean = false
+  popupFlag: boolean = false
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  public openCancelConfirm() {
+    this.popupFlag = true
+    this.confirmFlag = true
+  }
+
+  public confirmCancel() {
+  }
+
+  public closePopup() {
+    this.popupFlag = false
+    this.confirmFlag = false
   }
 
 }
