623e6bf20e7a179f488a5d5baea9dbc498a2aaba
Sven Moon <svenpire@gmail.com>
2021-05-15 15:55:13 -0400

    feat(post-pay): receipt styling
    - add stooge data if none present for visualization

--- src/app/modules/payment/components/pay-tx-result/pay-tx-result.component.ts
+++ src/app/modules/payment/components/pay-tx-result/pay-tx-result.component.ts
@@ -4,9 +4,15 @@ import { Store } from '@ngrx/store';
 import { Observable } from 'rxjs';
 import { OrderItemDetailed } from 'src/app/modules/order/models/Item';
 import { selectCartItemsWithIngredientInfo } from 'src/app/modules/order/state/cart/cart.selectors';
+import { selectOrderItemEntities, selectOrderItemIds } from 'src/app/modules/order/state/order-items/order-items.selectors';
 import { updateHeader } from '../../../shared/state/shared.actions';
 import { trxnResult } from '../../models/TrxnResult';
 import { selectPayResult } from '../../state/payment.selectors';
+import * as _ from 'lodash'
+import { addOrderItem } from 'src/app/modules/order/state/order-items/order-items.actions';
+import { addCartItem } from 'src/app/modules/order/state/cart/cart.actions';
+import { postPayment, updateCCInfo } from '../../state/payment.actions';
+import { ccData } from '../../models/CCData';
 
 @Component({
   selector: 'app-pay-tx-result',
@@ -29,19 +35,73 @@ export class PayTxResultComponent implements OnInit {
   ) { }
 
   ngOnInit(): void {
+    this.store.select(selectOrderItemEntities).subscribe(entities => {
+      if (_.size(entities) == 0) {
+        this.store.dispatch(addOrderItem({
+          orderItem: {
+
+            id: 'sandwich-1',
+            name: 'Test Sandwich 1',
+            ingredients: [
+              'sourdough',
+              'ham',
+              'cheddar'
+            ],
+            itemGroup: 'sandwich',
+            price: 9.69,
+            quantity: 1,
+            subtotal: 9.69,
+            viewDetail: false
+
+          }
+        }))
+        this.store.dispatch(addCartItem({ id: 'sandwich-1' }))
+        this.store.dispatch(addOrderItem({
+          orderItem: {
+
+            id: 'salad-1',
+            name: 'Test Salad 1',
+            ingredients: [
+              'mixed_greens',
+              'ham',
+              'blue_cheese'
+            ],
+            itemGroup: 'salad',
+            price: 13.50,
+            quantity: 2,
+            subtotal: 27.00,
+            viewDetail: false
+
+          }
+        }))
+        this.store.dispatch(addCartItem({ id: 'salad-1' }))
+      }
+    })
+
+    if (this.payResult$ == undefined) {
+      // this.router.navigate(['/pay'])
+      let ccInfo: ccData = {
+        name: 'abc132',
+        number: '1234567890',
+        cvv: '123',
+        exp: '1983-12-12',
+        amount: 15.02
+      }
+      this.store.dispatch(updateCCInfo({ data: ccInfo }))
+      this.store.dispatch(postPayment({ data: ccInfo }))
+
+    } else {
+      this.paySuccessFlag
+        ? this.store.dispatch(updateHeader({ header: 'Success!' }))
+        : this.store.dispatch(updateHeader({ header: 'Oops!' }))
+    }
+
     this.payResult$ = this.store.select(selectPayResult)
     this.payResult$.subscribe(result =>
       this.paySuccessFlag = result.status == 'approved'
         ? true
         : false
     )
-    if (this.result == undefined) {
-      this.router.navigate(['/pay'])
-    } else {
-      this.result.status === 'approved'
-        ? this.store.dispatch(updateHeader({ header: 'Success!' }))
-        : this.store.dispatch(updateHeader({ header: 'Oops!' }))
-    }
     this.items$ = this.store.select(selectCartItemsWithIngredientInfo)
   }
 
@@ -56,5 +116,10 @@ export class PayTxResultComponent implements OnInit {
   public closePopup() {
     this.popupFlag = false
     this.confirmFlag = false
+  }
+
+  // dev
+  public flipResults() {
+    this.paySuccessFlag = !this.paySuccessFlag
   }
 }
